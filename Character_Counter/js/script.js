0b9662bbb3bfa5a21e0f7ccbdf70529e096d3205
112233qk <Satrio.subakti77@gmail.com>
2026-05-27 17:29:49 +0700

    add changin color text based on the input limit

--- Character_Counter/js/script.js
+++ Character_Counter/js/script.js
@@ -1,10 +1,24 @@
-//Select DOM element from index.html
+//Select DOM element
 const count = document.getElementById("count");
 const output = document.getElementById("output");
 
+//Listen for user input
 count.addEventListener("input", () => {
   value = count.value;
   length = value.length;
 
   output.textContent = length;
+
+  //changing text color base on the limit
+  if (length >= 30) {
+    output.classList.add("limit");
+  } else if (length >= 20) {
+    output.classList.remove("warning", "danger", "limit");
+    output.classList.add("danger");
+  } else if (length >= 10) {
+    output.classList.remove("warning", "danger", "limit");
+    output.classList.add("warning");
+  } else {
+    output.classList.remove("warning", "danger", "limit");
+  }
 });
