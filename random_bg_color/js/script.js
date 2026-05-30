3a21faa986838de1d5cfb1a131690263e4bd803d
112233qk <Satrio.subakti77@gmail.com>
2026-05-30 17:21:57 +0700

    js

---
+++ random_bg_color/js/script.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("button");
+
+button.onclick = function () {
+  const num = Math.floor(Math.random() * 10) + 1;
+
+  switch (num) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      break;
+    case 8:
+      break;
+    case 9:
+      break;
+    case 10:
+      break;
+  }
+};
