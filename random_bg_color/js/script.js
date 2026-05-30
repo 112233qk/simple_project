6a13893e45ebd61b5797f29ec1708aaaafa298dc
112233qk <Satrio.subakti77@gmail.com>
2026-05-30 18:10:22 +0700

    adding comment for readability

--- random_bg_color/js/script.js
+++ random_bg_color/js/script.js
@@ -3,14 +3,15 @@ const text = document.getElementById("text");
 
 button.onclick = function () {
   const num = Math.floor(Math.random() * 10) + 1;
-  console.log(`${num}`);
 
-  for (i = 1; i <= 10; i++) {
+  // removing unused color class to prevent class stacking
+  for (let i = 1; i <= 10; i++) {
     document.body.classList.remove(`bg_${i}`);
   }
 
   document.body.classList.add(`bg_${num}`);
 
+  //changin text color for dark bg
   if (num == 2 || num == 9 || num == 7) {
     text.classList.add("white");
   } else {
