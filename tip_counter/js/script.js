4de4b503bc8a67b67b2a8d7c16eff0915005a405
112233qk <Satrio.subakti77@gmail.com>
2026-05-31 17:47:39 +0700

    more safety and slight change in css

--- tip_counter/js/script.js
+++ tip_counter/js/script.js
@@ -4,17 +4,25 @@ const submit = document.getElementById("submit");
 const result = document.getElementById("result");
 
 submit.onclick = function () {
-  const value = total.value;
-  const percent = tip.value;
+  const value = Number(total.value);
+  const percent = Number(tip.value);
 
+  // check the value if is within the limit
   if (percent < 0 || value < 0) {
     window.alert("tip or total cannot be under 0");
+    total.value = "";
+    tip.value = "";
+    result.textContent = ``;
   } else if (value == 0) {
-    window.alert("total cannot be 0");
+    window.alert("total cannot be 0 or empty");
+    result.textContent = ``;
   } else if (percent > 999 || value > 999999) {
     window.alert("total cannot exceed 999999 and 999%");
+    total.value = "";
+    tip.value = "";
+    result.textContent = ``;
   } else {
-    const count = value * (percent / 100);
+    const count = (value * (percent / 100)).toFixed(2);
     result.textContent = `your tip will be ${count}$`;
   }
 };
