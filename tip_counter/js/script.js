89850293e396ac68e213a449e44fcec1da4b714e
112233qk <Satrio.subakti77@gmail.com>
2026-05-31 17:36:25 +0700

    add safety to js and make change to css style

--- tip_counter/js/script.js
+++ tip_counter/js/script.js
@@ -7,6 +7,14 @@ submit.onclick = function () {
   const value = total.value;
   const percent = tip.value;
 
-  const count = value * percent;
-  result.textContent = `your tip will be ${count}`;
+  if (percent < 0 || value < 0) {
+    window.alert("tip or total cannot be under 0");
+  } else if (value == 0) {
+    window.alert("total cannot be 0");
+  } else if (percent > 999 || value > 999999) {
+    window.alert("total cannot exceed 999999 and 999%");
+  } else {
+    const count = value * (percent / 100);
+    result.textContent = `your tip will be ${count}$`;
+  }
 };
